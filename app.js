31e164e06583fdae589ed5ef22fbeaa56e432dfc
thomasbibi <thomasbibin38@gmail.com>
2022-12-08 17:23:25 +0530

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const postRouter = require('./routes/post');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 const cors = require('cors'); //middleware
-const cloudinary  = require('cloudinary').v2;
+// const cloudinary  = require('cloudinary').v2;
 
 dotenv.config();
-const port = process.env.PORT;
+const port = 8080
 const app = express();
-const mongodbUri = process.env.MONGODB_URI;
-console.log(mongodbUri)
-mongoose.connect(mongodbUri);
+// const mongodbUri = process.env.MONGODB_URI;
+// console.log(mongodbUri)
+// mongoose.connect(mongodbUri);
+
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone", ()=>console.log("Connected"), ()=>console.log("Error"))
 
 app.use(cors()); 
 app.use("/posts", postRouter)
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_API_CLOUD,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_API_CLOUD,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// })
 
 app.listen(port, () => console.log(`Server started listening on port ${port}`))
 module.exports = app;
--- routes/Post.js
+++ routes/Post.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const postModel = require('../models/post');
-const {UploadApiResponse,v2 } = require('cloudinary');
+const postModel = require('../models/Post');
+// const {UploadApiResponse,v2 } = require('cloudinary');
 const multer = require('multer');
 const router = express.Router();
 
-const storage = multer.diskStorage({})
-const upload =  multer({storage})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    /*Appending extension with original name*/
+    cb(null, file.originalname) 
+  }
+})
+
+var upload = multer({ storage: storage });
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({extended: true}));
@@ -23,47 +32,29 @@ router.get("/", async (req,res)=>{
     }
 });
 
-router.post("/create", upload.single('postimage') ,async (req,res)=>{
+router.post("/create",upload.single('postimage'),async(req,res)=>{
+
     try{
-        console.log("api called - get posts");
-        console.log(req.file.path);
-        if(!req.file){
-            return res.status(400).json({
-                status: "failed",
-                message : "please select file"
-            })
-        }
-
-//        console.log(req.file);
-
-        let uploadedFile = UploadApiResponse;
-        try{
-            uploadedFile = await v2.uploader.upload(req.file.path, {
-                folder: "instaclone",
-                resource_type: "auto"
-            });
-
-        }catch(err){
-            console.log("error in loading file to cloudinary :",err);
-        }
-//        console.log("loaded file to cloudinary URL is :",uploadedFile.url);
-        //create post obj from req body
-        const post = {
-            ...req.body,
-            imageurl: uploadedFile.url
-        }
-        console.log(post);
-        //add that obj in mongodb
-        const mongoRes= await postModel.create(post);
-        // //get res from mongodb and send that res
-        res.status(200).json({
-            status: "success",
-            data : mongoRes
-        });
-
-    }catch(err){
-        res.status(400).json({error : err.message})
+    const {name,location,description} = await req.body
+   
+    console.log(req.file)
+    const path = "http://127.0.0.1:8887/" + req.file.originalname
+     const post = await postModel.create({
+        "name" : name,
+         "location": location,
+         "description": description,
+         "likes": 0,
+         "date": new Date(),
+         "imageurl": path
+     })
+    res.status(200).json(post)
     }
-});
+    catch(e){
+        res.status(404).json({
+            status : "failed",
+            message : e.message
+        })
+    }
 
+})
 module.exports = router;
